1d51b4f021100ffe625c44ca63a35d7f6b170117
phaptran <phaptran112@gmail.com>
2021-10-10 01:38:19 +0700

    setup API with typescript

--- src/api/cityApi.ts
+++ src/api/cityApi.ts
@@ -1,9 +1,15 @@
 import axiosClient from './axiosClient';
+import { ListResponse, City } from 'models';
 
 const cityApi = {
-  getAll() {
+  getAll(): Promise<ListResponse<City>> {
     const url = '/cities';
-    return axiosClient.get(url);
+    return axiosClient.get(url, {
+      params: {
+        _page: 1,
+        _limit: 10,
+      },
+    });
   },
 };
 
