497da99527e728c25173e11d31619c4057d4d780
phaptran <phaptran112@gmail.com>
2021-10-11 18:47:05 +0700

    make admin page layout with MaterialUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React, { useEffect } from 'react';
-import { Switch, Route } from 'react-router-dom';
-
-import cityApi from 'api/cityApi';
-import LoginPage from 'features/auth/pages/Login';
-import { AdminLayout } from 'components/Layout';
 import { NotFound, PrivateRoute } from 'components/Common';
-import { useAppDispatch } from 'app/hooks';
-import { Button } from '@material-ui/core';
-import { authActions } from 'features/auth/authSlice';
+import { AdminLayout } from 'components/Layout';
+import LoginPage from 'features/auth/pages/Login';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    cityApi.getAll().then((response) => console.log(response));
-  });
-
   return (
     <>
-      <Button variant="contained" color="primary" onClick={() => dispatch(authActions.logout())}>
-        Logout
-      </Button>
       <div>
         <Switch>
           <Route path="/login">
