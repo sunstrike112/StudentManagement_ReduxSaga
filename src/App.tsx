3fba6fbeca4f9624da05d367661ec57a9568fe14
phaptran <phaptran112@gmail.com>
2021-10-11 12:30:52 +0700

    login and logout flow in saga

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,36 @@ import cityApi from 'api/cityApi';
 import LoginPage from 'features/auth/pages/Login';
 import { AdminLayout } from 'components/Layout';
 import { NotFound, PrivateRoute } from 'components/Common';
+import { useAppDispatch } from 'app/hooks';
+import { Button } from '@material-ui/core';
+import { authActions } from 'features/auth/authSlice';
 
 function App() {
+  const dispatch = useAppDispatch();
+
   useEffect(() => {
     cityApi.getAll().then((response) => console.log(response));
   });
 
   return (
-    <div>
-      <Switch>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <PrivateRoute path="/admin">
-          <AdminLayout />
-        </PrivateRoute>
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
-    </div>
+    <>
+      <Button variant="contained" color="primary" onClick={() => dispatch(authActions.logout())}>
+        Logout
+      </Button>
+      <div>
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <PrivateRoute path="/admin">
+            <AdminLayout />
+          </PrivateRoute>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </>
   );
 }
 
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,19 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-import createSagaMiddleware from 'redux-saga'
-import rootSaga from './rootSaga'
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './rootSaga';
+import authReducer from 'features/auth/authSlice';
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    auth: authReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(sagaMiddleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
 });
 
-sagaMiddleware.run(rootSaga)
+sagaMiddleware.run(rootSaga);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
