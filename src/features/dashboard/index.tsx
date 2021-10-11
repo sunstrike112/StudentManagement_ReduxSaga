47961a094d6037bc2e83c4119b0971823ac14159
phaptran <phaptran112@gmail.com>
2021-10-11 23:20:39 +0700

    handle dashboard data with saga

--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -1,5 +1,33 @@
-import * as React from 'react';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
+import React, { useEffect } from 'react';
+import {
+  dashboardActions,
+  selectDashboardLoading,
+  selectDashboardStatistics,
+  selectHightStudentList,
+  selectLowestStudentList,
+  selectRankingByCity,
+} from './dashboardSlice';
 
 export default function Dashboard() {
+  const dispatch = useAppDispatch();
+  const loading = useAppSelector(selectDashboardLoading);
+  const statistics = useAppSelector(selectDashboardStatistics);
+  const highestStudentList = useAppSelector(selectHightStudentList);
+  const lowestStudentList = useAppSelector(selectLowestStudentList);
+  const rankingByCityList = useAppSelector(selectRankingByCity);
+
+  console.log({
+    loading,
+    statistics,
+    highestStudentList,
+    lowestStudentList,
+    rankingByCityList,
+  });
+
+  useEffect(() => {
+    dispatch(dashboardActions.fetchData);
+  });
+
   return <div>Dashboard</div>;
 }
